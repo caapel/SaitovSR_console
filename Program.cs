f9813bfb9b47fe5ddfa852b48fd679bdbab6db8d
caapel <caapel@mail.ru>
2024-02-08 16:24:51 +0300

    Add *.cs-files

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace SaitovSR
+{
+    //Saitov Stanislav Radicovich, T-2-09
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Sqr(5));
+        }
+
+        static public double Sqr(double X)
+        { 
+            return Math.Pow(X, 2);
+        }
+    }
+}
